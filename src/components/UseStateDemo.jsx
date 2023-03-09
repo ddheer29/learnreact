980533eb4abcc1e597459f95adbb9d2d14008691
Divyang Dheer <88249922+ddheer29@users.noreply.github.com>
2023-03-09 20:45:26 +0530

    useStateHook Component tested

--- src/components/UseStateDemo.jsx
+++ src/components/UseStateDemo.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React, {useState} from 'react'
+import '../styles/UseStateDemo.css'
 
 const UseStateDemo = () => {
-  return (
-    <div style={{color: "black"}}>
-        <h1>UseStateDemo</h1>
-    </div>
-  )
+    const [num, setNum] = useState(1);
+    return (
+        <div className="App">
+            <div className='main'>
+                <h1 className='heading'>{num}</h1>
+                <div className="buttons">
+                    <button className="btn" onClick={(e) => (setNum(num+1))}>Increment</button>
+                    <button className="btn" onClick={(e) => (setNum(num-1))}>Decrement</button>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default UseStateDemo
